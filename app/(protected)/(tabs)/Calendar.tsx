a404ba622e9d43cefa308584265802361cdfd5ca
tereshonkov <tereshonkov.dima@gmail.com>
2025-11-26 15:39:37 +0200

    feat: Enhance Calendar component layout; add selected dates display and PDF download button

--- app/(protected)/(tabs)/Calendar.tsx
+++ app/(protected)/(tabs)/Calendar.tsx
@@ -1,15 +1,19 @@
 import CalendarComponent from "@/components/Calendar";
 import { LinearGradient } from "expo-linear-gradient";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import useDayPress from "@/hooks/useDayPress";
-
-const data = {
-  "2024-06-10": { startingDay: true, color: "#50cebb", textColor: "#ffffff" },
-  "2024-06-11": { color: "#70d7c7", textColor: "#ffffff" },
-};
+import Button from "@/components/ui/Button/Button";
 
 export default function Calendar() {
   const { selectedDates, markedDates, handleDayPress } = useDayPress();
+  const selectedDatesGrouped = () => {
+    const dates = Object.values(selectedDates);
+    if (dates.length === 0) return "Немає вибраних дат";
+    const firstDate = dates[0];
+    const lastDate = dates[dates.length - 1];
+    if (dates.length === 1) return firstDate;
+    return `(${firstDate} - ${lastDate})`;
+  }
   return (
     <LinearGradient
       colors={["#0D1B2A", "#1B263B", "#415A77"]}
@@ -25,9 +29,28 @@ export default function Calendar() {
           paddingVertical: 32,
         }}
       >
-        <View style={{ width: "90%" }}>
-          <CalendarComponent markedDates={markedDates} handleDayPress={handleDayPress} />
+        <View
+          style={{
+            width: "90%",
+            backgroundColor: "#FFFFFF",
+            borderRadius: 12,
+            padding: 16,
+            shadowColor: "#ffffffff",
+            shadowOffset: { width: 0, height: 5 },
+            shadowOpacity: 0.3,
+            shadowRadius: 5,
+          }}
+        >
+          <CalendarComponent
+            markedDates={markedDates}
+            handleDayPress={handleDayPress}
+          />
         </View>
+        <View style={{ marginTop: 24, alignItems: "center", backgroundColor: "#FFFFFF", padding: 16, borderRadius: 12, shadowColor: "#ffffffff", shadowOffset: { width: 0, height: 5 }, shadowOpacity: 0.3, shadowRadius: 5, }}>
+          <Text style={{ fontSize: 16, fontWeight: "600", marginBottom: 8 }}>Вибрані дати:</Text>
+          <Text style={{ fontSize: 14, textAlign: "center" }}>{selectedDatesGrouped()}</Text>
+        </View>
+        <Button style={{ marginTop: 24 }}>Завантажити PDF</Button>
       </View>
     </LinearGradient>
   );
