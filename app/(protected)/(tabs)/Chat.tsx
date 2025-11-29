2ffd91514dcfca9c6026b36621dfc33b21aaea6d
tereshonkov <tereshonkov.dima@gmail.com>
2025-11-29 10:01:17 -0800

    feat: enhance Chat component layout and add file upload and camera buttons; update ProgressBar max progress to 30

--- app/(protected)/(tabs)/Chat.tsx
+++ app/(protected)/(tabs)/Chat.tsx
@@ -1,9 +1,91 @@
-import { View, Text } from 'react-native'
+import { View, ScrollView, Pressable, Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { primaryColor, secondaryColor } from "@/constants/theme";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Chat() {
   return (
-    <View>
-        <Text>Chat Screen</Text>
-    </View>
-  )
+    <LinearGradient
+      colors={["#0D1B2A", "#1B263B", "#415A77"]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 0, y: 1 }}
+      style={{ flex: 1 }}
+    >
+      <ScrollView
+        contentContainerStyle={{ alignItems: "center", marginTop: 20 }}
+        style={{ flex: 1 }}
+      >
+        <View
+          style={{
+            justifyContent: "space-around",
+            alignItems: "center",
+            marginTop: 32,
+            backgroundColor: primaryColor,
+            padding: 8,
+            borderRadius: 100,
+            flexDirection: "row",
+            // width: "60%",
+          }}
+        >
+          <Pressable>
+            <View
+              style={{
+                alignItems: "center",
+                backgroundColor: "#FFFFFF",
+                borderRadius: 50,
+                flexDirection: "row",
+                padding: 4,
+                marginRight: 16,
+                width: 140,
+                justifyContent: "center",
+              }}
+            >
+              <Ionicons
+                name="add-circle-outline"
+                size={32}
+                color={primaryColor}
+              />
+              <Text
+                style={{
+                  color: primaryColor,
+                  fontSize: 12,
+                  marginLeft: 8,
+                  padding: 4,
+                  fontWeight: "700",
+                }}
+              >
+                Додати файл
+              </Text>
+            </View>
+          </Pressable>
+          <Pressable>
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "#FFFFFF",
+                borderRadius: 50,
+                flexDirection: "row",
+                padding: 4,
+                width: 140,
+              }}
+            >
+              <Ionicons name="camera-outline" size={32} color={primaryColor} />
+              <Text
+                style={{
+                  color: primaryColor,
+                  fontSize: 12,
+                  marginLeft: 8,
+                  padding: 4,
+                  fontWeight: "700",
+                }}
+              >
+                Камера
+              </Text>
+            </View>
+          </Pressable>
+        </View>
+      </ScrollView>
+    </LinearGradient>
+  );
 }
--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -4,7 +4,7 @@ import { useEffect, useRef } from "react";
 import AnimatedNumber from "./AnimatedNumber";
 
 const MAX_WIDTH = 300;
-const MAX_PROGRESS = 7;
+const MAX_PROGRESS = 30;
 
 export default function ProgressBar({ progress }: { progress: number }) {
   const animatedValue = useRef(new Animated.Value(0)).current;
@@ -22,7 +22,7 @@ export default function ProgressBar({ progress }: { progress: number }) {
     <View style={{ alignItems: "center", marginBottom: 40, gap: 4 }}>
         <View style={{flexDirection: "row", justifyContent: "space-between", width: MAX_WIDTH, marginBottom: 4}}>
             <Text style={{color: "#FFFFFF", fontWeight: "bold"}}>Pro-період:</Text>
-            <Text style={{color: "#C2C2C2"}}>залишилося днів <AnimatedNumber value={progress} /></Text>
+            <Text style={{color: "#C2C2C2"}}>залишилося днів <AnimatedNumber value={progress} /> / 30</Text>
         </View>
       <View
         style={{
